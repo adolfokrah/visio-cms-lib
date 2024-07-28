055d96941bb1acc17d2f0265614258259efe00a9
adolfokrah <adolfokrah@gmail.com>
2024-07-28 15:49:43 +0200

    fix: added init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, Label, Input } from "../lib/main";
+
+function App() {
+  return (
+    <>
+      <Label>Name</Label>
+      <Input placeholder="enter something" />
+      <Button>click me</Button>
+    </>
+  );
+}
+
+export default App;
