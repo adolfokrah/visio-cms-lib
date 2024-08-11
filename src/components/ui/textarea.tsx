3c3af57b1acc982d50287ec9f2297136e4930ac0
adolfokrah <adolfokrah@gmail.com>
2024-08-11 13:17:01 +0200

    feat: added custom scroll bar to textarea

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
   return (
     <textarea
       className={cn(
-        'visio-cms-flex visio-cms-font-regular visio-cms-min-h-[60px] visio-cms-w-full visio-cms-rounded-md visio-cms-border visio-cms-border-input visio-cms-bg-dark-900 visio-cms-px-3 visio-cms-py-2 visio-cms-text-xs visio-cms-shadow-sm placeholder:visio-cms-text-muted-foreground focus-visible:visio-cms-outline-none disabled:visio-cms-cursor-not-allowed disabled:visio-cms-opacity-50',
+        'visio-cms-flex scrollbar-custom visio-cms-font-regular visio-cms-min-h-[60px] visio-cms-w-full visio-cms-rounded-md visio-cms-border visio-cms-border-input visio-cms-bg-dark-900 visio-cms-px-3 visio-cms-py-2 visio-cms-text-xs visio-cms-shadow-sm placeholder:visio-cms-text-muted-foreground focus-visible:visio-cms-outline-none disabled:visio-cms-cursor-not-allowed disabled:visio-cms-opacity-50',
         className,
       )}
       ref={ref}
