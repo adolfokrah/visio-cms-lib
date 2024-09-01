c85e59b0ccc4e9001784af3ce92f1c0788ff60d3
adolfokrah <adolfokrah@gmail.com>
2024-09-01 16:42:28 +0200

    fix: added copy page for each locale

--- src/components/layout/canvas/components/language-controls.tsx
+++ src/components/layout/canvas/components/language-controls.tsx
@@ -10,7 +10,7 @@ import { usePagesState } from '@/lib/states/usePagesState';
 import { useProjectConfigurationState } from '@/lib/states/useProjectConfigState';
 export default function LanguageControls() {
   const { pages, setPages } = usePagesState();
-  const { supportedLanguages } = useProjectConfigurationState();
+  const { supportedLanguages, defaultLanguage } = useProjectConfigurationState();
   const activePage = pages.find((page) => page.active);
 
   return (
@@ -25,10 +25,34 @@ export default function LanguageControls() {
           <DropdownMenuItem
             key={language}
             onClick={() => {
-              const newPages = pages.map((page) => ({
-                ...page,
-                activeLanguageLocale: activePage?.name === page.name ? locale : page.activeLanguageLocale,
-              }));
+              const newPages = pages.map((page) => {
+                const data = {
+                  ...page,
+                  activeLanguageLocale: activePage?.name === page.name ? locale : page.activeLanguageLocale,
+                  blocks: {
+                    ...page.blocks,
+                    [locale]:
+                      (page.blocks?.[locale]?.length ?? 0) < 1
+                        ? page.blocks?.[defaultLanguage?.locale || ''] || []
+                        : page.blocks?.[locale] || [],
+                  },
+                };
+                if (!page?.seo?.[locale]) {
+                  data.seo = {
+                    ...page.seo,
+                    [locale]: {
+                      meta: {
+                        title: page.seo?.[defaultLanguage?.locale || '']?.meta?.title || '',
+                        description: page.seo?.[defaultLanguage?.locale || '']?.meta?.description || '',
+                        keywords: page.seo?.[defaultLanguage?.locale || '']?.meta?.keywords || '',
+                        featuredImage: page.seo?.[defaultLanguage?.locale || '']?.meta?.featuredImage || '',
+                      },
+                    },
+                  };
+                }
+                return data;
+              });
+
               setPages(newPages);
             }}
           >
