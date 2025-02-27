7a33a1156e504ec1e06a405d4f3408d697453d2e
adolfokrah <adolfokrah@gamil.com>
2025-02-27 14:28:34 +0100

    update

--- visio.config.ts
+++ visio.config.ts
@@ -1,5 +1,6 @@
 import blocks from './src/components/blocks';
-const visioConfig = {
+import {  ProjectConfig } from './src/lib/types';
+const visioConfig:ProjectConfig = {
   blocks,
   allowImageTransformation: false,
   supportedLanguages: [
@@ -22,6 +23,7 @@ const visioConfig = {
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9leHN4ZHNmaXdlendodW5hZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0Mzc5ODIsImV4cCI6MjA1NTAxMzk4Mn0.R6Kk8nMl39IcMKSgpgG0EaGnuxECEOufFTE-vC-BegE',
   unsplashAccessKey: 'Rw6b_NBqAXZV4wTUOF7A2QtagLtB9apC2dQYZco1RUU',
+  
 };
 
 export default visioConfig;
